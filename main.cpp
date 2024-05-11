1e7ad41452033b25ce92293e024198643d552397
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-11 17:35:26 +0300

    ex. 3 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
-//dev
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int user_num, filtered_number = 0, multiplier = 1,digit;
+
+	cout << "Enter number: ";
+	cin >> user_num;
+	for (user_num; user_num > 0; user_num /= 10)
+	{
+		digit = user_num % 10;
+		if (user_num % 10 != 3 && user_num % 10 != 6) {
+			filtered_number += digit * multiplier;
+			multiplier *= 10;
+		}
+	}
+	cout << "Your number wihout 3 && 6: "<< filtered_number<<endl;
 	system("pause");
 	return 0;
 }
