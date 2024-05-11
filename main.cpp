2b7c47979c8fb27988951d5c32b91238328458cf
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-11 16:06:57 +0300

    ex.1 lab had done

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Guess the number" << endl;
+	std::cout << "Guess the number" << endl;
 	srand(time(0));
 	int	rund_num = rand() % 500 + 1;
 	int attempts = 0, user_num;
@@ -15,31 +15,29 @@ int main()
 	
 	for (attempts;; attempts++)
 	{
-		cout << "Enter your number(1-500,"
+		std::cout << "Enter your number(1-500,"
 			<<" 0-stop program):\t";
 		cin >> user_num;
 
-		if (user_num !=0) {
-			if (user_num == rund_num) {
-				cout << "You won" << endl;
+		if (user_num == 0) {
+			std::cout << "Game over" << endl;
+			break;
+		}
+
+		else if (user_num == rund_num) {
+			std::cout << "You won" << endl;
 				attempts++;
 				break;
 				
 			}
-			else if (user_num > rund_num) {
-				cout << "My number is smaller\n" 
+		else if (user_num > rund_num) {
+			std::cout << "My number is smaller\n"
 					<< endl;
 			}
-			else {
-				cout << "My number is greater\n" 
-					<< endl;
-			}
-		}
-
 		else {
-			cout << "Game over"<< endl;
-			break;
-		}
+			std::cout << "My number is greater\n"
+					<< endl;
+			}
 
 	}
 	
@@ -50,14 +48,14 @@ int main()
 	auto duration = end_time - start_time;
 
 	// Displaying the result in seconds
-	cout << "\nTime: " 
+	std::cout << "\nTime: "
 		<<chrono::duration_cast
 		<chrono::seconds>(duration).count() 
 		<< " sec\n";
 
 	
 	
-	cout <<"Attempts: "<< attempts << endl;
+	std::cout <<"Attempts: "<< attempts << endl;
 	system("pause");
 	return 0;
 }
