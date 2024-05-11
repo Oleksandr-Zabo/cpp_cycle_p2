49842698c45168e57a8b8ab2dc2f30c3ad4bfb77
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-11 18:06:30 +0300

    ex. 5 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-//dev
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int user_num, digit_sum = 0;
+	cout << "Enter number: ";
+	cin >> user_num;
+	int num_sqare = pow(user_num, 2);
+
+	for (int i = user_num; i != 0;) {
+		digit_sum += i % 10;
+		i /= 10;
+	}
+	digit_sum = pow(digit_sum, 3);
+
+	cout << digit_sum << " " << num_sqare << endl;
+	cout << bool(num_sqare == digit_sum)<<endl;
 	system("pause");
 	return 0;
 }
