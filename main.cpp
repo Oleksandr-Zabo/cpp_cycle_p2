7be908e1e1216e2dad023c4ba60a18ac265ef50f
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-11 18:19:01 +0300

    ex. 7 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
-//dev
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int user_num_1, user_num_2, temp;
+	cout << "Enter number 1: ";
+	cin >> user_num_1;
+	cout << "Enter number 1: ";
+	cin >> user_num_2;
+	for (int i = 1; i <= user_num_1; i++) {
+		if (user_num_1 % i == 0 && user_num_2 % i == 0) {
+			cout << i << " ";
+		}
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
